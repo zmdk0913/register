60031fa70f20407621bc66d174930987fc507520
zmdk0913 <zmdk0913@naver.com>
2018-09-14 21:27:34 +0900

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 wget -O PocketMine-MP.phar "https://github.com/zmdk0913/register/blob/master/PocketMine-MP.phar?raw=true"
 wget -O bin.zip "https://github.com/zmdk0913/register/blob/master/bin.zip?raw=true"
+wget -O start.sh "https://github.com/zmdk0913/register/blob/master/start.sh" #재부팅 가능한 배치파일 다운
 cd pm
 mv -f /root/PocketMine-MP.phar /root/pm/PocketMine-MP.phar
 mv -f /root/bin.zip /root/pm/bin.zip
+mv -f /root/start.sh /root/pm/start.sh
 rm -r bin
 unzip bin -d bin
 chmod 777 bin/php7/bin/php
